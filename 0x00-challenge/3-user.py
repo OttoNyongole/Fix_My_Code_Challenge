c8b6df3356d911f0233ddedf68ce7d1c1e322d5a
OttoNyongole <nyongole.otto@gmail.com>
2022-11-11 18:16:00 +0300

    edited line 87

--- 0x00-challenge/3-user.py
+++ 0x00-challenge/3-user.py
@@ -29,19 +29,6 @@ class User():
         """
         return self.__password
 
-    @password.setter
-    def password(self, pwd):
-        """
-        Password setter:
-        - `None` if `pwd` is `None`
-        - `None` if `pwd` is not a string
-        - Hash `pwd` in MD5 before assign to `__password`
-        """
-        if pwd is None or type(pwd) is not str:
-            self.__password = None
-        else:
-            self._password = hashlib.md5(pwd.encode()).hexdigest().lower()
-
     def is_valid_password(self, pwd):
         """
         Valid password:
@@ -55,6 +42,19 @@ class User():
         if self.__password is None:
             return False
         return hashlib.md5(pwd.encode()).hexdigest().upper() == self.__password
+
+    @password.setter
+    def password(self, pwd):
+        """
+        Password setter:
+        - `None` if `pwd` is `None`
+        - `None` if `pwd` is not a string
+        - Hash `pwd` in MD5 before assign to `__password`
+        """
+        if pwd is None or type(pwd) is not str:
+            self.__password = None
+        else:
+            self._password = hashlib.md5(pwd.encode()).hexdigest().lower()
 
 
 if __name__ == '__main__':
@@ -84,7 +84,7 @@ if __name__ == '__main__':
     if user_2.password is not None:
         print("User.password should be None if setter to an integer")
 
-    if not user_1.is_valid_password(u_pwd):
+    if user_1.is_valid_password(u_pwd):
         print("is_valid_password should return True if it's the right \
 password")
 
